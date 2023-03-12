44693c38c6c766309e9c0729b37759f896145410
Nshtk <slava2034@gmail.com>
2023-03-12 16:51:22 +0300

    Update lab1

--- lab1/cache.py
+++ lab1/cache.py
@@ -1,16 +1,24 @@
 class LRUCache:
     __dict = None
+    __capacity = None
 
     def __init__(self, capacity: int = 10) -> None:
+        self.__capacity = capacity
         self.__dict = dict.fromkeys((range(capacity)))
 
     def get(self, key: str) -> str:
-        return self.__dict[key]
+        if key in self.__dict:
+            return self.__dict[key]
+        else:
+            assert key in self.__dict[key], 'Value not found'
+            return ""
 
     def set(self, key: str, value: str) -> None:
+        if len(self.__dict) > self.__capacity:
+            self.__dict.pop(next(iter(self.__dict)))
         self.__dict[key] = value
-        pass
 
     def rem(self, key: str) -> None:
-        self.__dict[key] = ''
-        pass
+        assert key in self.__dict[key], 'Key not found'
+        if key in self.__dict:
+            del self.__dict[key]
